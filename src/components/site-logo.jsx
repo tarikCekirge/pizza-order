2de78527a4659e1189bdd3db54c01557c46b8296
Tarık Çekirge <tcekirge@gmail.com>
2025-03-21 15:40:03 +0300

    Added home success pages

--- src/components/site-logo.jsx
+++ src/components/site-logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
 
 const SiteLogo = () => {
     const alt = 'logo'
     return (
-        <a href="/" className='max-w-sm block'>
+        <NavLink to="/" className='max-w-[300px] md:max-w-sm block'>
             <img src={logo} alt={alt} className='w-full' />
-        </a>
+        </NavLink>
     )
 }
 
